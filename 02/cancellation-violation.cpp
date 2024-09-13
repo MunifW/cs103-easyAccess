a021e847cff17d786ad0a2f80f152566c5c69a96
MunifW <wmunif123@gmail.com>
2024-09-13 17:00:51 -0400

    Worked on cancelllation-violation

--- 02/cancellation-violation.cpp
+++ 02/cancellation-violation.cpp
@@ -13,6 +13,36 @@ int main()
 	 * fixed amount of space to write the exponent, and there is also
 	 * a fixed amount of space for the coefficient... */
 
+	double d = 0.5;
+	double e = (d+d+d)/3;
+	double f = 5;
+	double dummyE = d + d + d;
+
+	if (d + f == e + f)
+	{
+		cout << "Congrats, you found the number 5!" << "\n";
+	}
+	else
+	{
+		cout << "YOU SUCK!" << "\n";
+	}
+
+	if (d != e)
+	{
+		cout << "They are not equal <-- You want this." << "\n";
+	}
+	else 
+	{
+		cout << "This is equal" << "\n";
+	}
+
+	cout << "e: "<< e << "\n";
+	cout << "e + f: "<< e + f << "\n";
+	cout << "d + f: "<< d + f << "\n";
+
+	printf("d = %.17f\n", d);
+	printf("e = %.17f\n", e);
+
 	return 0;
 }
 
