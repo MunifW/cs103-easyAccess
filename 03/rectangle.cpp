b4a409132fb38f4f7080d9319f103a45ec4554c1
MunifW <wmunif123@gmail.com>
2024-09-29 01:13:38 -0400

    Pushing rectangle code

--- 03/rectangle.cpp
+++ 03/rectangle.cpp
@@ -11,6 +11,71 @@ using std::cout;
 
 int main()
 {
+	int n = 0;
+	int m = 0;
+
+	cout << "Please input a number for n: " << "\n";
+	cin >> n;
+
+	cout << "Please input a number for m: " << "\n";
+	cin >> m;
+
+#if 0 //Code for regular rectangle
+	for (int i = 0; i < n; i++)
+	{
+		cout << "*";
+
+		for (int j = 0; j < m; j++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+#endif
+
+	// for (int i = 0; i < n; i++) 
+	// {
+	// 	for (int j = 0; j < m; j++) 
+	// 	{
+	// 		printf("(%i,%i) ",i,j);
+	// 		// cout << "(" << i << "," << j << ") ";
+	// 	}
+	// 	printf("\n");
+	// }
+
+	/*
+	I want:
+	ijjj
+	i  j
+	ijjj
+	*/
+
+	for (int i = 0; i < n; i++) //Controls rows
+	{
+		//cout << "*";
+
+		cout << "i";
+
+		for (int j = 0; j < m; j++) //Controls columns
+		{
+			//cout << "*";
+			cout << "j";
+
+			if (i == 1)
+			{
+				cout << "k";
+			}
+			else
+			{
+				//cout << " ";
+			}
+
+		}
+
+		cout << "\n";
+	}
+
+
 	return 0;
 }
 
