325641cdce41d2bede3653557f42b4e939b9a294
MunifW <wmunif123@gmail.com>
2024-10-05 06:37:30 -0400

    Worked on second-smallest

--- 03/second-smallest.cpp
+++ 03/second-smallest.cpp
@@ -7,16 +7,75 @@
 using std::cin;
 using std::cout;
 
+/*My current thought process for finding the smallest integer from an inputted list is to have the user input a number for x
+	and then continously input numbers for y, but for each input, x will be compared with y and its value will be reassigned
+		over and over again, until the smallest number is found.*/
+
 int main()
 {
 	/* First figure out how to print out the smallest integer first. */
 
-	int x;
+	int x, y, z;
 
-	while (cin >> x)
+#if 0
+	while (true)
 	{
-		cout << "Current x value: " << x << "\n";
+		cout << "Please input a value for x: " << "\n";
+		cin >> x;
+
+		cout << "Please input a value for y: " << "\n";
+		cin >> y;
+
+		//cout << "Current y value: " << y << "\n";
+
+		if (x < y)
+		{
+			z = x;
+
+			//cout << "Current z value (for x < y) " << z << "\n";
+		}
+		else if (y < x)
+		{
+			z = y;
+
+			//cout << "Current z value (for y < x) " << z << "\n";
+		}
+
+		if (!cin >> x || !cin >> y)
+		{
+			break;
+		}
 	}
+#endif
+	/*Basic idea is that x and y will always be overwritten into z. 
+	
+	Noticable problem right now is that previous inputs will be thrown away, which makes finding smallest int not possible
+		IDEA: Might have to use z in a conditional to compare with x and y... Maybe if (z < x || y)*/
+
+	cout << "Please input a value for x: " << "\n"; //X will be a static # that gets compared to
+	cin >> x;
+
+	cout << "Please input a value for y: " << "\n"; //Will always be changing
+
+	while (cin >> y)
+	{
+		if (x < y) //z will become x if x < y
+		{
+			z = x;
+
+			//cout << "Current z value (for x < y) " << z << "\n";
+		}
+		else if (y < x) //z will become y is y < x
+		{
+			z = y;
+
+			//cout << "Current z value (for y < x) " << z << "\n";
+		}
+
+		//cout << "Current Z value: " << z << "\n";
+	}
+
+	cout << "Smallest Value: " << z << "\n";
 
 	return 0;
 }
