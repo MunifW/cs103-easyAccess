ac672050362969af574f68bd49538eb7ced42103
MunifW <wmunif123@gmail.com>
2024-10-16 20:17:01 -0400

    Did the reverse problem

--- 05/reverse.cpp
+++ 05/reverse.cpp
@@ -10,9 +10,52 @@ using std::cout;
 using std::vector;
 
 /* your answer goes here... */
+void reverse(vector<int> V)
+{	
+	int values;
+	//vector<int> V;
+	vector<int> V2;
+
+	cout << "Please input values for the vector. Input -1 to stop: " << "\n";
+
+	while (true)
+	{
+		cin >> values;
+
+		if (values == -1)
+		{
+			break;
+		}
+
+		V.push_back(values);
+	}
+
+	for (int i = V.size() - 1; 0 <= i; i--)
+	{
+		//cout << "Values of V: " << V[i] << "\n";
+
+		V2.push_back(V[i]);
+		V.pop_back();
+	}
+
+	for (int j = 0; j < V2.size(); j++)
+	{
+		V.push_back(V2[j]);
+	}
+
+
+	for (int k = 0; k < V.size(); k++)
+	{
+		cout << "Values of V: " << V[k] << "\n";
+	}
+
+
+}
 
 int main()
 {
+	vector<int> V;
+	reverse(V);
 	/* TODO: call your function, make sure it works... */
 	return 0;
 }
