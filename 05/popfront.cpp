29c421145a9148aab4a9b49937882d79e45ed266
MunifW <wmunif123@gmail.com>
2024-10-25 06:41:21 -0400

    Adding popfront progress

--- 05/popfront.cpp
+++ 05/popfront.cpp
@@ -13,11 +13,49 @@ using std::vector;
 /* your answer goes here: */
 void pop_front(vector<int>& V)
 {
+	int values = 0;
+	vector<int> swap;
+
+	cout << "Please input values for your vector. Type -1 to stop inputting: " << "\n";
+
+	while (true)
+	{
+		cin >> values;
+
+		if (values == -1)
+		{
+			break;
+		}
+
+		V.push_back(values);
+	}
+
+	for (int i = V.size() - 1; 0 <= i; i--) //This for loop needs to put the reverse integers into swap, then use another for loop to 
+	{
+		//cout << "Current values in vector V: " << V[i] << "\n";
+		swap.push_back(swap[i]); //This does not work because swap is still empty as of right now.
+		swap.pop_back();
+	}
+
+	for (int k = 0; k < swap.size(); k++)
+	{
+
+		cout << "Values of swap: " << swap[k] << "\n";
+	}
+
+	for (int j = 0; j < V.size(); j++)
+	{
+		cout << "New vector V values: " << V[j] << "\n";
+	}
 }
 
 int main()
 {
 	/* TODO: call your function, make sure it works... */
+	vector<int> myVec;
+
+	pop_front(myVec);
+
 	return 0;
 }
 
