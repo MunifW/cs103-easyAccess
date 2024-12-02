41c2cc910bebaccf2a3a1a666cd06a625edf509a
MunifW <wmunif123@gmail.com>
2024-12-01 21:31:01 -0500

    Finish wc pls

--- 06/wc.cpp
+++ 06/wc.cpp
@@ -14,11 +14,44 @@ using std::endl;
 #include <string>
 using std::string;
 
-/* your answer goes here... */
+/* A word should be classified as anything with a space before or after it. 
+	-> So I would need to keep a count of all the whitespaces, and add to it. */
+
+// I want the program to see that a space surrounded by letters mean it's a word
+// I want it to exclude any space that is not between letters 
+
+void wordCount(string& input)
+{
+	int count = 0;
+	int temp = 0;
+
+	for (size_t i = 0; i < input.size(); i++)
+	{
+		temp++;
+
+		if ((input[i] >= 65 && input[i] <= 90) || (input[i] >= 97 && input[i] <= 122) || input[i] == 44) // This defines a word is of letters from the alphabet
+		{
+			if (input[i + 1] == ' ')
+			{
+				count++;
+			}
+		}
+	}
+
+	//count--;
+
+	cout << "Current count: " << count + 1 << "\n";
+}
 
 int main()
 {
-	/* TODO: call your function, make sure it works... */
+	string userInput;
+
+	cout << "Please input a sentence or word!" << "\n";
+	getline(cin, userInput);
+
+	wordCount(userInput);
+
 	return 0;
 }
 
